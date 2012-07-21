3c0b6aaa29b63133ad1c165ba37e5515517f0b0a
Michal Miskernik <michal.miskernik@gmail.com>
2012-07-21 19:24:40 +0200

    removed slowing down image requests

--- server.js
+++ server.js
@@ -7,12 +7,6 @@ app.use(app.router);
 app.use(express.static(__dirname + '/public'));
 
 app.get('/sweetwar.js', pkg.server);
-app.get('/*.png', function (req, res, next) {
-  var time = Math.random() * 3000 + 1000;
-  setTimeout(function () {
-    next();
-  }, time);
-});
 
 var port = process.argv[2] || 8080;
 
